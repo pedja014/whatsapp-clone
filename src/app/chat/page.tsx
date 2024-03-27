7d050e4d53ed97c04375267f00401a16fce0e002
Predrag Milicevic <predragmilicevic@gmail.com>
2024-03-27 15:08:56 +0100

    Connect Clerk and Stream user

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,49 +1,57 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { StreamChat } from "stream-chat";
 import {
   Channel,
   ChannelHeader,
+  ChannelList,
   Chat,
+  LoadingIndicator,
   MessageInput,
   MessageList,
   Thread,
   Window,
 } from "stream-chat-react";
-
-const userId = "user_2dk0diJvNownhqelku5OAkr3p8a";
-
-const chatClient = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_KEY!);
-
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yZGswZGlKdk5vd25ocWVsa3U1T0FrcjNwOGEifQ.towAJdgMKTpYm7vGzzLSa1sYvSF2tepW2r6abGs94uQ";
-
-chatClient.connectUser(
-  {
-    id: userId,
-    name: "PEraPeric",
-  },
-  token
-);
-
-const channel = chatClient.channel("messaging", "channel_1", {
-  name: "Channel #1",
-  members: [userId],
-});
+import useInitializeChatClient from "./useInitializeChatClient";
 
 export default function ChatPage() {
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoadingIndicator size={40} />
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div className="h-screen">
       <Chat client={chatClient}>
-        <Channel channel={channel}>
-          <Window>
-            <ChannelHeader />
-            <MessageList />
-            <MessageInput />
-          </Window>
-          <Thread />
-        </Channel>
+        <div className="flex h-full flex-row">
+          <div className="w-full max-w-[300px]">
+            <ChannelList
+              filters={{
+                type: "messaging",
+                members: { $in: [user.id] },
+              }}
+              sort={{ last_message_at: -1 }}
+              options={{ state: true, presence: true, limit: 10 }}
+            />
+          </div>
+          <div className="hful w-full">
+            <Channel>
+              <Window>
+                <ChannelHeader />
+                <MessageList />
+                <MessageInput />
+              </Window>
+              <Thread />
+            </Channel>
+          </div>
+        </div>
       </Chat>
     </div>
   );
